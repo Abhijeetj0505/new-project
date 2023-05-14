071001af2b24d6905ea193181751912715adb8aa
Abhijeetj0505 <78902006+Abhijeetj0505@users.noreply.github.com>
2023-05-14 13:27:17 +0530

    Add files via upload

---
+++ client_test.py
@@ -0,0 +1,28 @@
+import unittest
+from client3 import getDataPoint
+
+class ClientTest(unittest.TestCase):
+    def test_getDataPoint_calculatePrice(self):
+        quotes = [
+            {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        bid_price = quotes[0]['top_bid']['price']
+        ask_price = quotes[0]['top_ask']['price']
+        price = (bid_price + ask_price) / 2
+        self.assertEqual(getDataPoint(quotes), (price, 'ABC'))
+
+    def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+        quotes = [
+            {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        bid_price = quotes[0]['top_bid']['price']
+        ask_price = quotes[0]['top_ask']['price']
+        price = (bid_price + ask_price) / 2
+        self.assertEqual(getDataPoint(quotes), (price, 'ABC'))
+
+    """ ------------ Add more unit tests ------------ """
+
+if __name__ == '__main__':
+    unittest.main()
